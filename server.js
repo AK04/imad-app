4d25828c0c124f649c39469cf289a6ac5fa9659a
Albin Kottaram <albindoesgames@gmail.com>
2017-08-22 20:54:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,14 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
+
 app.get('/ui/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
